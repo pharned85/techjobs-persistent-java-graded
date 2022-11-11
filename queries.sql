75a2f9985ee19d8a34921e8684db7dcb10983284
pharned85 <harned.exchange@gmail.com>
2022-11-10 18:05:30 -0600

    Done with part 3 I think

--- queries.sql
+++ queries.sql
@@ -2,7 +2,9 @@
  SELECT *
  FROM job;
 -- Part 2: Test it with SQL
-
+ SELECT name
+ FROM employer
+ WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
-
+DROP TABLE job;
 -- Part 4: Test it with SQL
