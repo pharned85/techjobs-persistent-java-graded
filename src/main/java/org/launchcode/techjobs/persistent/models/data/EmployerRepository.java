ec6189c02a87e994259310ffc2035656ef4f2758
pharned85 <harned.exchange@gmail.com>
2022-11-13 12:58:29 -0600

    Trying to get test 3 to pass

--- src/main/java/org/launchcode/techjobs/persistent/models/data/EmployerRepository.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/data/EmployerRepository.java
@@ -4,7 +4,10 @@ import org.launchcode.techjobs.persistent.models.Employer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
+@Transactional
 public interface EmployerRepository extends CrudRepository<Employer, Integer> {
     Iterable<Employer> findAllOrderByName(String name);
 }
